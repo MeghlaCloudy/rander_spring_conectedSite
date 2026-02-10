611a31eae718eb5a57777943b494eb2af7d13d91
Meghla Cloudy <meghlaclo@gmail.com>
2026-02-10 18:59:00 +0600

    ..

--- shajgoj/lib/api_ex/services/api_config.dart
+++ shajgoj/lib/api_ex/services/api_config.dart
@@ -1,7 +1,9 @@
 class ApiConfig {
   static const String baseUrlold = "https://rander-secqurity-3.onrender.com/api";
 
-  static const String baseUrl = "http://192.168.20.151:8080/api";
+  static const String baseUrl = "https://new-rander-spring-global.onrender.com/api";
+
+  static const String baseUrlold2 = "http://192.168.20.151:8080/api";
 
 
 
